612e5be4f9b6c2a2bb3854f0166e80d0740f7f03
Ali Firas <ali.albdaer.py@gmail.com>
2024-04-09 13:57:22 +0300

    initial commit

---
+++ exe.py
@@ -0,0 +1,29 @@
+import os
+from shutil import rmtree
+
+
+REMOVE_SPEC = True
+REMOVE_BUILD = True
+
+
+scripts = ("server.py", "client.py")
+
+
+for script in scripts:
+    try: 
+        print(f'Generating executable for {script}...')
+        os.system(f'python -m PyInstaller {script} --onefile --distpath /apps')
+
+        if REMOVE_SPEC:
+            print(f'Removing {script[:-3]}.spec file...')
+            os.remove(f'{script[:-3]}.spec')
+
+        if REMOVE_BUILD:
+            print('Removing build directory...')
+            rmtree('build')
+
+    except Exception as e:
+        print(f'Error generating executable for {script}: {e}')
+
+    else:
+        print(f'Executable file generated: apps/{script[:-3]}.exe')
